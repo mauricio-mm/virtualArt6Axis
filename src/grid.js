4ea362a6fd51191410fff7dfa693ed9a89968ef1
mauricio-mm <mauricio.moura@acad.pucrs.br>
2026-09-11 00:37:42 -0300

    offset grid

--- src/grid.js
+++ src/grid.js
@@ -2,6 +2,7 @@ import * as THREE from "../node_modules/three/build/three.module.js";
 
 export function createGrid() {
   const grid = new THREE.GridHelper(10, 10, 0xe8edf2, 0xb8c0ca);
+  grid.position.y = -0.5;
   grid.material.opacity = 0.72;
   grid.material.transparent = true;
 
